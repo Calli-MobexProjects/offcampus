7e06c85ef0d0b0324f5537963e2d37251a4a3436
LORD KAY <offeilord@gmail.com>
2017-05-29 14:53:11 +0000

    Started with the analytics page

---
+++ admin/analytics.php
@@ -0,0 +1,20 @@
+<?php
+include "../navbars/home_navbar.php";
+require_once '../inc/connection.php';
+?>
+
+<head>
+	<title>OCTPs&reg; | Analytics</title>
+</head>
+<body>
+	<div class="main-analytics">
+		<div class="row">
+			<div class="col s2 m2 l2">
+				<?php include '../navbars/sidenav.php'; ?>
+			</div>
+			<div class="col s10 m10 l10">
+				<div class="card-panel"></div>
+			</div>
+		</div>
+	</div>
+</body>

42d1fb0d2519dafc1396f5e37882c257ea54c23c
LORD KAY <offeilord@gmail.com>
2017-05-31 16:30:45 +0000

    Fixed the signup error

--- signup.php
+++ signup.php
@@ -2,61 +2,73 @@
 	<title>OCTPs&reg; | Register</title>
 </head>
 <?php
+  // $Time=date("Y/m/d h:i:sa"); 
 session_start();
 ini_set( 'error_reporting', E_ALL);
 ini_set( 'display_errors', true );
 include("navbars/navbar.php");
 require_once "inc/connection.php";
 
-if (isset($_POST['proceed'])) 
-{
-  //Accessing the user input from the form
-	$student_id = trim($_POST['student_id']);
-	$firstname = trim($_POST['firstname']);
-	$lastname  = trim($_POST['lastname']);
-	$othername = trim($_POST['othername']);
-	$course    = trim($_POST['courseTitle']);
-	$email     = trim($_POST['email']);
-	$password  = trim($_POST['password']);
-	//Encrypting the user password using crypt
-	$encryptpass = md5($password);
+	if(isset($_POST['proceed'])) 
+		{
+			$student_id = trim($_POST['student_id']);
+			$firstname	= trim($_POST['firstname']);
+			$lastname 	= trim($_POST['lastname']);
+			$othername 	= trim($_POST['othername']);
+			$course 	= trim($_POST['courseTitle']);
+			$email 		= trim($_POST['email']);
+			$password 	= $_POST['password'];
+			# code...
+			#Encrypting the password for the users
+			$encryptpass = md5($password);
+			$phone   = trim($_POST['phone']);
 
-	$phone 	   = trim($_POST['phone']);
-	$student  = "student";
-	//Using default image from the dir in the parent collections
-	$pic = "images/boys.jpg";
-	$department = "N/A";
+			//Assigning default values to department and Profile
+			$department = "N/A";
+			$picture	= "images/boys.jpg";
+			$profile 	= "student";
 
-    
-    $Time=date("Y/m/d h:i:sa"); 
-    
-	$qs = "INSERT INTO register (Stud_id,f_Name,l_Name,other_Name,program,department,picture,	password,phone,email,Profile,date_Created,login_time) VALUES('$student_id','$firstname','$lastname','$othername','$course','$department','$pic','$encryptpass','$phone','$email','student','$Time','')";
+			//inserting data into the database
+			$signup_query = "INSERT INTO register VALUES('$student_id','$firstname','$lastname','$othername','$course','$department','$picture','$encryptpass','$phone','$email','$profile','2017-05-30 05:08:12','2017-05-30 05:08:12')";
 
+			$signup_result = $mysqli->query($signup_query);
 
-	//Executing the queries
-	$res = $mysqli->query($qs);
-	if ($res)
-	{
-		 echo 
-	   		' 	<script>
-	   				var alert = $("<span>Registration Successfully Done</span>");
-	        		Materialize.toast(alert, 3500, "rounded");
-	   			</script>
-	   		';
+			if ($signup_result)
+			{
+				?>
+				<script type="text/javascript">
+					$.sweetModal({
+						icon:$.sweetModal.ICON_SUCCESS,
+						content: 'Registration Successfull',
+						width:'400px',
+						timeout:2800
 
-		header("Location:index.php");
-	}
-	else
-	{
-		echo 
-   		'
-   			<script>
-   				var alert = $("<span>Error Occurred While Registering. Contact the Administrator</span>");
-        		Materialize.toast(alert, 3500, "rounded");
-   			</script>
-   		';
-	}
-}
+					});
+					setTimeout(function() {
+						window.location = "index.php";
+					}, 3000);
+				</script>
+				<?php
+			}
+			else
+			{
+				?>
+				<script type="text/javascript">
+					$.sweetModal({
+						icon:$.sweetModal.ERROR,
+						content: 'Error Occurred, Contact System Admin',
+						width:'400px',
+						timeout:2800
+
+					});
+					setTimeout(function() {
+						window.location = "signup.php";
+					}, 3000);
+				</script>
+				<?php
+			}
+		}
+
 ?>
 <body>
 	<main class="signup_body">
@@ -69,30 +81,30 @@ if (isset($_POST['proceed']))
 				</div>
 				<div class="col s12 m8 l8 offset-m2 offset-l2">
 					<div class="card-panel col s12 form_main z-depth-2">
-						<form method="post" action="signup.php">
+						<form method="post" action="signup.php" id="regForm">
 							<div class="input-field col s12 m12 l12" style="padding-top: 15px;">
 								  <i class="material-icons prefix">credit_card</i>
-						          <input id="icon_prefix" type="text" name="student_id" class="validate" data-length="14" required="required">
-						          <label for="icon_prefix">Student ID</label>
+						          <input id="student_id" type="text" name="student_id" class="validate" data-length="14" required="required">
+						          <label for="student_id">Student ID</label>
 							</div>
 							<div class="input-field col s12 m6 l6">
 								  <i class="material-icons prefix">person_pin</i>
-						          <input id="icon_prefix" type="text" name="firstname" class="validate" data-length="60" required="required">
-						          <label for="icon_prefix">First Name</label>
+						          <input id="firstname" type="text" name="firstname" class="validate" data-length="60" required="required">
+						          <label for="firstname">First Name</label>
 							</div>
 							<div class="input-field col s12 m6 l6">
 								  <i class="material-icons prefix">person_pin</i>
-						          <input id="icon_prefix" type="text" name="lastname" class="validate" data-length="60" required>
-						          <label for="icon_prefix">Last Name</label>
+						          <input id="lastname" type="text" name="lastname" class="validate" data-length="60" required>
+						          <label for="lastname">Last Name</label>
 							</div>
 							<div class="input-field col s12 m6 l6">
 								  <i class="material-icons prefix">person_pin</i>
-						          <input id="icon_prefix" type="text" name="othername" class="validate" data-length="60">
-						          <label for="icon_prefix">Other Name(Option)</label>
+						          <input id="othername" type="text" name="othername" class="validate" data-length="60">
+						          <label for="othername">Other Name(Option)</label>
 							</div>
 							<div class="input-field col s12 m6 l6">
 								<i class="material-icons prefix">book</i>
-								 <select name="courseTitle" required="required">
+								 <select name="courseTitle" required="required" id="course">
 									<option value="" disabled selected>Select A Course</option>
 										<optgroup label="Education Courses">
 											<?php 
@@ -117,19 +129,19 @@ if (isset($_POST['proceed']))
 							</div>
 							<div class="input-field col s12 m12 l12">
 								  <i class="material-icons prefix">email</i>
-						          <input id="icon_prefix" type="email" name="email" class="validate" data-length="60" required="required">
-						          <label for="icon_prefix">Email</label>
+						          <input id="email" type="email" name="email" class="validate" data-length="60" required="required">
+						          <label for="email">Email</label>
 						         
 							</div>
 							<div class="input-field col s12 m6 l6">
 								  <i class="material-icons prefix">lock</i>
-						          <input id="icon_prefix" type="password" name="password" class="validate" data-length="20" required>
-						          <label for="icon_prefix">Password</label>
+						          <input id="password" type="password" name="password" class="validate" data-length="20" required>
+						          <label for="password">Password</label>
 							</div>
 							<div class="input-field col s12 m6 l6">
 								  <i class="material-icons prefix">phone</i>
-						          <input id="icon_prefix" type="number" name="phone" class="validate" data-length="20" required>
-						          <label for="icon_prefix">Phone</label>
+						          <input id="phone" type="number" name="phone" class="validate" data-length="20" required>
+						          <label for="phone">Phone</label>
 							</div>
 							<div class="input-field col s12 m12 l12">
 								<button type="submit" name="proceed" id="preload-btn" class="btn light-blue accent-3 waves-effect waves-light z-depth-2 send">Proceed <i class="material-icons right">arrow_forwarded</i></button>
@@ -143,9 +155,9 @@ if (isset($_POST['proceed']))
 		<script type="text/javascript">
 			$(document).ready(function(){
 				$('select').material_select();
+
 			});
 		</script>
-
 	</main>
 	<?php
 	include("footer/reg_footer.php");

77201a77ab4e5b2f5edcbb3a6fc42a581c8f8181
LORD KAY <offeilord@gmail.com>
2017-06-01 20:16:50 +0000

    Added the profile page for the admin portal

---
+++ admin/profile.php
@@ -0,0 +1,17 @@
+<?php 
+
+include '../navbars/home_navbar.php';
+
+?>
+<head>
+	<title>OCTPs&reg; | Admin Profile</title>
+</head>
+<body>
+	<div class="profile-body">
+		<div class="row">
+			<div class="container">
+				
+			</div>
+		</div>
+	</div>
+</body>

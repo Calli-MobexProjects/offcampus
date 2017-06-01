bad924578bf4d76811be5d4a729425e447f4e029
LORD KAY <offeilord@gmail.com>
2017-06-01 20:17:15 +0000

    Changed the student sidenav to suit nice one

---
+++ navbars/stud_sidenav.php
@@ -0,0 +1,23 @@
+<div class="navigation" style="border-right: 1px solid #e8e8e8;">
+	<ul class="nav-demo">
+		<li id="h_menu_item" class="active" ><a href="#home"><i class="material-icons left icon blue-text">home</i><span class="menu_name">Home</span></a></li>
+		<li id="a_menu_item"><a href="#apply"><i class="material-icons left icon orange-text">insert_drive_file</i><span class="menu_name">Apply Now</span></a></li>
+		<li id="d_menu_item"><a href="#download_assessment"><i class="material-icons left icon green-text">file_download</i><span class="menu_name">Assessment Form</span></a></li>
+
+		<div class="divider"></div>
+		<li class="grey-text text-accent-1 header disabled">Supervision</li>
+		<li><a href="#"><i class="material-icons left icon">supervisor_account</i><span class="menu_name">Super. Details</span></a></li>
+		<li><a href="#"><i class="material-icons left icon">list</i><span class="menu_name">Incentives</span></a></li>
+		<div class="divider"></div>
+		<li class="grey-text text-accent-1 header disabled">Settings</li>
+		<li><a href="../admin/change_password.php"><i class="material-icons left icon">lock</i><span class="menu_name">Change Password</span></a></li>
+		<li class="grey-text text-accent-1 header disabled">Discussions</li>
+		<li><a href="#"><i class="material-icons left icon">forum</i><span class="menu_name">Forum</span></a></li>
+
+		<div class="divider"></div>
+		<li class="grey-text text-accent-1 header" disabled="disabled">Report Issues ?</li>
+		<li><a href="#"><i class="material-icons left icon">info</i><span class="menu_name">Help</span></a></li>
+		<li><a href="#"><i class="material-icons left icon">message</i><span class="menu_name">Feedback</span></a></li>
+		
+	</ul>
+</div>

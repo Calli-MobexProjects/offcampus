7f54dfad40b25af551fba47bb05a6767f391f1c0
LORD KAY <offeilord@gmail.com>
2017-06-03 22:41:10 +0000

    Added a file for the management and addition of admins

---
+++ admin/manage_admin.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+<head>
+	<title>OCTPs&reg; | Manage Admin</title>
+</head>
+<body>
+	<div class="madmin-body">
+		<div class="row">
+			<div class="col s2 m2 l2">
+				<?php include '../navbars/sidenav.php';?>
+			</div>
+			<div class="col s10 m10 l10"></div>
+		</div>
+	</div>
+</body>

b1cc87f64707fa8a4c92777d63cc4b2f0a6b1b1b
LORD KAY <offeilord@gmail.com>
2017-06-12 10:57:39 +0000

    Made some sick mods for the students page --> Now looking nice and sweet

--- admin/students.php
+++ admin/students.php
@@ -73,6 +73,46 @@
 		a#edit{
 
 		}
+		div.template{
+			border-radius:3px;
+		}
+		span.status{
+			position: absolute;
+			z-index: 9999;
+			margin-left: -25px;
+			border-radius: 20px;
+			color:white;
+			font-size: 14px;
+			font-weight: 600;
+			padding-right: 10px;
+			padding-top: 3px;
+		}
+		div.cont_size{
+			margin-left: -12px;
+			margin-right: -12px;
+		}
+		.mt2{
+			margin-top: 2px;
+		}
+		span.stud_det{
+			margin-left: 10px;
+			font-weight: 500;
+			font-size: 14px;
+			text-shadow: 1px 1px 1px #919191;
+			text-transform: capitalize;
+		}
+		i.lo{
+			margin-top: -2px;
+			margin-left: 2px;
+		}
+		div#student_body{
+			display: none;
+		}
+		div#preloader{
+			position: relative;
+			z-index: 99999;
+			top: -6px;
+		}
 
 	</style>
 </head>
@@ -82,10 +122,15 @@
 			<div class="col s2 m2 l2" id="side-pane">
 				<?php include '../navbars/sidenav.php'; ?>
 			</div>
-			<div class="col s10 m10 l10 animated fadeIn">
+			<div class="col s10 m10 l10">
+				 <div id="preloader">
+	                 <div class="progress" id="loader">
+	                      <div class="indeterminate blue"></div>
+	                  </div>
+                </div>
 				<div class="content_body">
 					<div class="row">
-						<div class="col s12 m12 l12">
+						<div class="col s12 m12 l12 animated fadeIn" id="student_body">
 							<h5 class="grey-text text-accent-3 left-align stud_list_title"><i class="material-icons left view">view_list</i>Students' List</h5>
 							<!-- Selecting the students from the database -->
 							<?php
@@ -108,26 +153,23 @@
 											$student_image = substr($stud_firstname,0,4);
 											
 											?>
-											<div class="card-panel z-depth-0 waves-effect waves-block waves-ripple col s12 stud_list" id="<?php echo "$stud_id";?>">
-												<div class="col s2 m2 l2" id="img">
-													<span class="image"><img src="../<?php echo "$stud_image";?>" alt="avatar" class="responsive-img circle"></span>
+											<div class="col s12 m2 l2">
+												<div id="<?php echo "$stud_id";?>" class="card-panel col s12 waves-effect waves-block waves-ripple template">
+													<div class="cont_size" id="<?php echo "$user_identifier";?>">
+														<div class="stud_avatar">
+															<img src="../images/admin/boy.svg" class="responsive-img mt2"/>
+														</div>
+														<span class="stud_det text-center small-text center-align">
+														 <?php echo "$stud_firstname"." "."$stud_lastname";?>
+														</span><br>
+														<!-- <span class="stud_det text-center small-text text-center">
+														 Date Here
+														</span><br> -->
+													</div>
 												</div>
-												<div class="col s3 m3 l3" id="name">
-													<span class="name left-align"><?php echo "$stud_firstname"." ".$stud_lastname."".$stud_othername;?></span>
-												</div>
-												<div class="col s2 m2 l2" id="phone">
-													<span class="phone"><?php echo "$stud_id";?></span>
-												</div>
-												<div class="col s3 m3 l3" id="email">
-													<span class="email left-align"><a href="#"><?php echo "$stud_email";?></a></span>
-												</div>
-												
-												<div class="col s2 m2 l2">
-													<span class="side_menu">
-														Date Here
-													</span>
-												</div>
+												<span id="<?php echo "$user_identifier"; ?>" class="status z-depth-1 waves-effect waves-block waves-ripple light-blue accent-3"><i class="material-icons left lo">my_location</i>Mapper</span>
 											</div>
+											
 											<script type="text/javascript">
 													$(document).ready(function(){
 														$("#edit_form").css("margin-top","-150px");
@@ -152,7 +194,7 @@
 																	\				<img src="../<?php echo "$stud_image";?>" style="width:80px; height=80px;border:4px solid rgba(0,0,0,0.25);" class="circle responsive-img">\
 																				</div>\
 																	\			<div class="col s8 m8 l8">\
-																	\				<span class="name" style="position:absolute;top:50px;left:135px;"><?php echo "$stud_firstname"." "."$stud_lastname"." "."$stud_othername";?></span>\
+																	\				<span class="name" style="position:absolute;top:50px;left:135px;text-transform:capitalize;"><?php echo "$stud_firstname"." "."$stud_lastname"." "."$stud_othername";?></span>\
 																	\				<span class="more_vert"><a href="" style="position:absolute;top:20px;right:60px;"><i class="material-icons right grey-text text-darken-2">delete</i></a><a href="" style="position:absolute;top:20px;right:100px;"><i class="material-icons right grey-text text-darken-2">archive</i></a></span>\
 																				<div>\
 																			</div>',
@@ -160,30 +202,30 @@
 																	content: '<form action="" method="post">\
 																                <div class="input-field col s12" style="margin-top:-10px;margin-bottom:8px;">\
 																	                <i class="material-icons prefix" id="icon_prefix">person_pin</i>\
-																	                <input type="text" name="firstname" class="validate" id="firstname" value="<?php echo "$stud_firstname"." "."$stud_lastname".""."$stud_othername";?>" disabled><label for="firstname" class="active">First Name</label>\
+																	                <input type="text" name="firstname" class="validate" id="firstname" value="<?php echo "$stud_firstname"." "."$stud_lastname".""."$stud_othername";?>" disabled style="font-size:14px;font-weight:400;line-height:1;color:#919191;"><label for="firstname" class="active">First Name</label>\
 																                </div>\
 																                <div class="input-field col s12" style="margin-top:-10px;margin-bottom:8px;">\
 																	                <i class="material-icons prefix" id="icon_prefix">credit_card</i>\
-																	                <input type="text" name="id" class="validate" value="<?php echo "$stud_id";?>" disabled>\
+																	                <input type="text" name="id" class="validate" value="<?php echo "$stud_id";?>" disabled style="font-size:14px;font-weight:400;line-height:1;color:#919191;">\
 																	                <label for="icon_prefix" class="active">ID</label>\
 																                </div>\
 																                 <div class="input-field col s12" style="margin-top:-10px;margin-bottom:8px;">\
 																	                <i class="material-icons prefix" id="icon_prefix">email</i>\
-																	                <input type="email" name="email" class="validate" value="<?php echo "$stud_email";?>" disabled>\
+																	                <input type="email" name="email" class="validate" value="<?php echo "$stud_email";?>" disabled style="font-size:14px;font-weight:400;line-height:1;color:#919191;">\
 																	                <label for="icon_prefix" class="active">Email</label>\
 																                </div>\
 																                 <div class="input-field col s12" style="margin-top:-10px;margin-bottom:8px;">\
 																	                <i class="material-icons prefix" id="icon_prefix">book</i>\
-																	                <input type="email" name="email" class="validate" value="<?php echo "$stud_program";?>" disabled>\
+																	                <input type="email" name="email" class="validate" value="<?php echo "$stud_program";?>" disabled style="font-size:14px;font-weight:400;line-height:1;color:#919191;">\
 																	                <label for="icon_prefix" class="active">Program</label>\
 																                </div>\
 																                 <div class="input-field col s12" style="margin-top:-10px;margin-bottom:8px;">\
 																	                <i class="material-icons prefix" id="icon_prefix">store</i>\
-																	                <input type="email" name="email" class="validate" value="<?php echo "$stud_department";?>" disabled>\
+																	                <input type="email" name="email" class="validate" value="<?php echo "$stud_department";?>" disabled style="font-size:14px;font-weight:400;line-height:1;color:#919191;">\
 																	                <label for="icon_prefix" class="active">Department</label>\
 																                </div>\
 																           </form>',
-																	width  : '680px'
+																	width  : '600px'
 																});
 														});
 
@@ -200,7 +242,7 @@
 										'<div class="card-panel z-depth-0 col s12 m12 l12" style="background-color:#eeeeee;">
 											<div class="empty_list">
 												<div class="image_list">
-													<img src="../images/admin/empty_list.svg" class="responsive-img" alt="Empty List"/>
+													<img src="../images/admin/list.svg" class="responsive-img" alt="Empty List"/>
 													<span class="empty_title">No Lecturer Added</span>
 												</div>
 
@@ -211,17 +253,16 @@
 								
 								$results->close();
 							?>
-							
 						</div>
 					</div>
 				</div>
-					<div class="fixed-action-btn" style="right: 70px;bottom: 40px;">
-					    <a  href class="btn-floating btn-large pulse light-blue accent-3">
+					<div id="view" class="fixed-action-btn" style="right: 70px;bottom: 40px;">
+					    <a  href class="btn-floating btn-large light-blue accent-3">
 					      <i class="large material-icons tooltipped" data-tooltip="View" data-position="left" data-delay="5">remove_red_eye</i>
 					    </a>
 					    <ul>
-					      <li><a class="btn-floating green accent-3 tooltipped" data-tooltip="Approved Student Letters" data-position="left" data-delay="5"><i class="material-icons">done_all</i></a></li>
-					      <li><a class="btn-floating amber darken-3 tooltipped" data-tooltip="Pending Student Letters" data-position="left" data-delay="5"><i class="material-icons">more_horiz</i></a></li>
+					      <li><a id="approved" class="btn-floating green accent-3 tooltipped pulse" data-tooltip="Approved Student Letters" data-position="left" data-delay="5"><i class="material-icons">done_all</i></a></li>
+					      <li><a id="pend" class="btn-floating amber darken-3 tooltipped" data-tooltip="Pending Student Letters" data-position="left" data-delay="5"><i class="material-icons">timeline</i></a></li>
 					    </ul>
 
 				    </div>
@@ -237,8 +278,27 @@
 														
 						}
 					?>
+					<script type="text/javascript">
+						$("a#pend").on('click',function(){
+							$("div#student_body").load("pending_letters.php");
+						});
+						$("a#approved").on('click',function(){
+							$("div#student_body").load("done_letters.php");
+						});
+
+						$(window).load(function(){
+				              setTimeout(function() {
+				                 $("div#preloader").fadeOut('slow');
+				              }, 2000);
+				              setTimeout(function() {
+				                $("div#student_body").fadeIn('slow');
+				              }, 2000);
+				                
+				            });
+					</script>
 		
 			</div>
+
 			<!-- Modal for the editting dialog page -->
 			<div id="modal">
 			    <!-- Page content -->

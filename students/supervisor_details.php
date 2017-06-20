df3cb4f5403d52f1953f22bc83734fee0666aadb
LORD KAY <offeilord@gmail.com>
2017-06-20 12:25:47 +0000

    Added some animation effects to the supervisor details page at the student

--- students/supervisor_details.php
+++ students/supervisor_details.php
@@ -2,9 +2,14 @@
  require_once '../inc/connection.php'; 
  ?>
  <body>
- 	<div class="row animated fadeIn">
+ 	<div class="row animated fadeInUp" id="super_body">
  		<div class="col s12 m12 l12">
- 			
+ 			<div class="col s12 m7 l7">
+ 				<div class="card-panel"></div>
+ 			</div>
+ 			<div class="col s12 m5 l5">
+ 				<div class="card-panel"></div>
+ 			</div>
  		</div>
  	</div>
  	<script type="text/javascript">

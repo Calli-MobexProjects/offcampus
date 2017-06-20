31fe765e48143c8f58d6689047b7ddfe3635a54e
LORD KAY <offeilord@gmail.com>
2017-06-20 10:38:07 +0000

    Still workin on the activites.js for the effects on the students page

--- libs/js/activities.js
+++ libs/js/activities.js
@@ -13,20 +13,22 @@
 
 
   $("#h_menu_item").click(function(){
-    if ($("#main_nav").hasClass("orange darken-3") || $("#main_nav").hasClass("green")) 
+    if ($("#main_nav").hasClass("orange darken-3") || $("#main_nav").hasClass("green") || $("#main_nav").hasClass("code-alliance")) 
     {
       $("#main_nav").removeClass("orange darken-3");
       $("#main_nav").removeClass("green");
+      $("#main_nav").removeClass("code-alliance");
       $("#main_nav").addClass("light-blue accent-3");
       $("#main_nav").addClass("animated fadeIn");
 
     }
 
   /* Checking the css for side nav */
-    if ($("#a_menu_item").hasClass("grey lighten-2") || $("#d_menu_item").hasClass("grey lighten-2")) 
+    if ($("#a_menu_item").hasClass("grey lighten-2") || $("#d_menu_item").hasClass("grey lighten-2") ||$("#sup_menu_item").hasClass("grey lighten-2")) 
     {
       $("#a_menu_item").removeClass("grey lighten-2").addClass("grey lighten-3");
       $("#d_menu_item").removeClass("grey lighten-2").addClass("grey lighten-3");
+      $("#sup_menu_item").removeClass("grey lighten-2").addClass("grey lighten-3");
       $("#h_menu_item").removeClass("grey lighten-3").addClass("grey lighten-2");
     }
     
@@ -34,43 +36,67 @@
   });
 
   $("#a_menu_item").click(function(){
-    if ($("#main_nav").hasClass("green") || $("#main_nav").hasClass("light-blue accent-3")) 
+    if ($("#main_nav").hasClass("green") || $("#main_nav").hasClass("light-blue accent-3") || $("#main_nav").hasClass("code-alliance")) 
     {
       $("#main_nav").removeClass("green");
       $("#main_nav").removeClass("light-blue accent-3");
       $("#main_nav").addClass("orange darken-3");
       $("#main_nav").addClass("animated fadeIn");
+      $("#main_nav").removeClass("code-alliance");
     }
     
     /* Checking the css for side nav */
-    if ($("#h_menu_item").hasClass("grey lighten-2") || $("#d_menu_item").hasClass("grey lighten-2")) 
+    if ($("#h_menu_item").hasClass("grey lighten-2") || $("#d_menu_item").hasClass("grey lighten-2") || $("#sup_menu_item").hasClass("grey lighten-2")) 
     {
       $("#d_menu_item").removeClass("grey lighten-2").addClass("grey lighten-3");
       $("#h_menu_item").removeClass("grey lighten-2").addClass("grey lighten-3");
+      $("#sup_menu_item").removeClass("grey lighten-2").addClass("grey lighten-3");
       $("#a_menu_item").removeClass("grey lighten-3").addClass("grey lighten-2");
     }
     
   });
 
   $("#d_menu_item").click(function(){
-    if ($("#main_nav").hasClass("orange darken-3") || $("#main_nav").hasClass("light-blue accent-3")) 
+    if ($("#main_nav").hasClass("orange darken-3") || $("#main_nav").hasClass("light-blue accent-3") || $("#main_nav").hasClass("code-alliance")) 
     {
       $("#main_nav").removeClass("orange darken-3");
       $("#main_nav").removeClass("light-blue accent-3");
+      $("#main_nav").removeClass("code-alliance");
       $("#main_nav").addClass("green");
       $("#main_nav").addClass("animated fadeIn");
     }
 
   /* Checking the css for side nav */
-    if ($("#h_menu_item").hasClass("grey lighten-2") || $("#a_menu_item").hasClass("grey lighten-2")) 
+    if ($("#h_menu_item").hasClass("grey lighten-2") || $("#a_menu_item").hasClass("grey lighten-2") || $("#sup_menu_item").hasClass("grey lighten-2")) 
     {
       $("#h_menu_item").removeClass("grey lighten-2").addClass("grey lighten-3");
       $("#a_menu_item").removeClass("grey lighten-2").addClass("grey lighten-3");
+      $("#sup_menu_item").removeClass("grey lighten-2").addClass("grey lighten-3");
       $("#d_menu_item").removeClass("grey lighten-3").addClass("grey lighten-2");
     }
     
   });
 
-  /* configuring the preloader for the body tags */
+  //Supervisor and incentives details navbar combo 
+   $("#sup_menu_item").click(function(){
+    if ($("#main_nav").hasClass("orange darken-3") || $("#main_nav").hasClass("light-blue accent-3") || $("#main_nav").hasClass("green")) 
+    {
+      $("#main_nav").removeClass("orange darken-3");
+      $("#main_nav").removeClass("light-blue accent-3");
+      $("#main_nav").removeClass("green");
+      $("#main_nav").addClass("code-alliance");
+      $("#main_nav").addClass("animated fadeIn");
+    }
+
+    if ($("#h_menu_item").hasClass("grey lighten-2") || $("#a_menu_item").hasClass("grey lighten-2") || $("#d_menu_item").hasClass("grey lighten-2")) 
+    {
+      $("#h_menu_item").removeClass("grey lighten-2").addClass("grey lighten-3");
+      $("#a_menu_item").removeClass("grey lighten-2").addClass("grey lighten-3");
+      $("#d_menu_item").removeClass("grey lighten-2").addClass("grey lighten-3");
+      $("#sup_menu_item").removeClass("grey lighten-3").addClass("grey lighten-2");
+    }
+    
+  });
+  
 
 });

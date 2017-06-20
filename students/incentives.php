d44c7e2f7dc56e3ed2a893036d461393c8acadf4
LORD KAY <offeilord@gmail.com>
2017-06-20 12:24:57 +0000

    added some animation effects to the incentives.php file

--- students/incentives.php
+++ students/incentives.php
@@ -3,13 +3,13 @@
  
 ?>
 <body>
-	<div class="incent_body">
-		<div class="row animated fadeIn">
-			<div class="col s12 m12 l12">
-				
-			</div>
+	
+	<div class="row animated fadeInUp" id="incent_body">
+		<div class="col s12 m12 l12">
+			<div class="card-panel"></div>
 		</div>
 	</div>
+
 	<script type="text/javascript">
 		document.title = 'OCTPs | Incentive Details';
 	</script>
